6d6fa63e98385d6eed1e7c818ad878bdc4b0935f
Andrew Dornbush <andrew.dornbush@gmail.com>
2018-03-22 12:29:45 -0400

    begin large reorganization of monolithic rcta package into standalone packages

---
+++ src/roman_joint_trajectory_controller.cpp
@@ -0,0 +1,259 @@
+#include "roman_joint_trajectory_controller.h"
+
+// system includes
+#if BUILD_LIVE_JPL
+#include <robot/robot_pub_types.h>
+#include <plan/plan_types.h>
+#include <plan/plan_utils.h>
+#endif
+#include <roman_client_ros_utils/RomanSpec.h>
+
+#if BUILD_LIVE_JPL
+// translate roman_spec_t to RomanSpec
+void translate_jplspec_to_rosspec(
+    roman_spec_t& spec,
+    roman_client_ros_utils::RomanSpec& spec_rosmsg)
+{
+    spec_rosmsg.waypoints.clear();
+    spec_rosmsg.valid.clear();
+
+    spec_rosmsg.utime = ros::Time::now().toNSec() * 1e3;
+    spec_rosmsg.num_mechanisms = ROBOT_NUM_MECHS;
+    for (int m = 0; m < ROBOT_NUM_MECHS; m++) {
+        spec_rosmsg.valid.push_back(spec.valid[m]);
+    }
+    spec_rosmsg.num_waypoints  = spec.num_waypoints;
+    spec_rosmsg.waypoints.resize(spec_rosmsg.num_waypoints);
+
+    const int64_t min_wp_time = 80000; // 46000
+    for(int w=0; w<spec_rosmsg.num_waypoints; w++)
+    {
+        roman_client_ros_utils::RomanWaypoint& wpt = spec_rosmsg.waypoints[w];
+        wpt.utime = (int64_t)spec.waypoints[w].timestamp;
+        if (w > 0) {
+            roman_client_ros_utils::RomanWaypoint& pwp = spec_rosmsg.waypoints[w - 1];
+            const int64_t diff_time = wpt.utime - pwp.utime;
+            if (diff_time < min_wp_time) {
+                ROS_INFO("Deadbanding time");
+                wpt.utime = pwp.utime + min_wp_time;
+
+                // shift all timestamps by the amount we "added in" here
+                const int64_t shift_time = wpt.utime - (int64_t)spec.waypoints[w].timestamp;
+                for (int ww = w; ww < spec.num_waypoints; ++ww) {
+                    spec.waypoints[w].timestamp += shift_time;
+                }
+            }
+        }
+
+        if (w % 10 == 0) {
+            ROS_INFO("Waypoint Time (%d): %ld", w, wpt.utime);
+            ROS_INFO_STREAM("Waypoint Time (" << w << "): " << wpt.utime);
+        }
+        wpt.num_joints = ROBOT_NUM_JOINTS;
+        for (int j = 0; j < ROBOT_NUM_JOINTS; j++) {
+            wpt.positions.push_back(spec.waypoints[w].positions[j]);
+        }
+        wpt.world2robot.position.x    = spec.waypoints[w].world2robot.pos.x;
+        wpt.world2robot.position.y    = spec.waypoints[w].world2robot.pos.y;
+        wpt.world2robot.position.z    = spec.waypoints[w].world2robot.pos.z;
+        wpt.world2robot.orientation.w = spec.waypoints[w].world2robot.rot.u;
+        wpt.world2robot.orientation.x = spec.waypoints[w].world2robot.rot.x;
+        wpt.world2robot.orientation.y = spec.waypoints[w].world2robot.rot.y;
+        wpt.world2robot.orientation.z = spec.waypoints[w].world2robot.rot.z;
+//        spec_rosmsg.waypoints.push_back(wpt);
+    }
+
+    for(int w=0; w<spec_rosmsg.num_waypoints; w++)
+    {
+        if (w % 10 == 0) {
+            ROS_INFO("Waypoint Time (%d): %ld", w, spec_rosmsg.waypoints[w].utime);
+        }
+    }
+
+    return;
+}
+
+
+void translate_rosspec_to_jplspec(
+    roman_client_ros_utils::RomanSpec& spec_rosmsg,
+    roman_spec_t& jplspec)
+{
+    memcpy(jplspec.valid, &spec_rosmsg.valid[0], sizeof(int) * ROBOT_NUM_MECHS);
+    jplspec.num_waypoints =  spec_rosmsg.num_waypoints;
+    for (int w = 0; w < jplspec.num_waypoints; w++) {
+        jplspec.waypoints[w].timestamp= w * 1e5; //spec_rosmsg.waypoints[w].utime;
+        if (w % 10 == 0) {
+            ROS_INFO("Waypoint Time (%d): %ld", w, jplspec.waypoints[w].timestamp);
+        }
+        memcpy(jplspec.waypoints[w].positions, &spec_rosmsg.waypoints[w].positions[0], sizeof(double) * ROBOT_NUM_JOINTS);
+        jplspec.waypoints[w].world2robot.pos.x = spec_rosmsg.waypoints[w].world2robot.position.x;
+        jplspec.waypoints[w].world2robot.pos.y = spec_rosmsg.waypoints[w].world2robot.position.y;
+        jplspec.waypoints[w].world2robot.pos.z = spec_rosmsg.waypoints[w].world2robot.position.z;
+        jplspec.waypoints[w].world2robot.rot.u = spec_rosmsg.waypoints[w].world2robot.orientation.w;
+        jplspec.waypoints[w].world2robot.rot.x = spec_rosmsg.waypoints[w].world2robot.orientation.x;
+        jplspec.waypoints[w].world2robot.rot.y = spec_rosmsg.waypoints[w].world2robot.orientation.y;
+        jplspec.waypoints[w].world2robot.rot.z = spec_rosmsg.waypoints[w].world2robot.orientation.z;
+    }
+}
+
+void spec_update_times(roman_client_ros_utils::RomanSpec& spec_rosmsg)
+{
+  double max_vel = 0.75;
+  double accel = 1.5;
+  roman_spec_t spec;
+  translate_rosspec_to_jplspec(spec_rosmsg,spec);
+  // updates utime according to a joint level trapezoidal profiling scheme
+  plan_utils_update_times_roman(&spec, max_vel, accel);
+  translate_jplspec_to_rosspec(spec, spec_rosmsg);
+}
+#endif
+
+RomanJointTrajectoryController::RomanJointTrajectoryController(
+    const std::string& ns)
+:
+    m_nh(),
+    m_server(
+        ros::NodeHandle(ns), "follow_joint_trajectory", false),
+    m_roman_spec_pub(),
+    m_roman_spec_reply_sub()
+{
+    m_roman_spec_pub = m_nh.advertise<roman_client_ros_utils::RomanSpec>(
+            "roman_spec", 1);
+    m_roman_spec_reply_sub = m_nh.subscribe(
+            "roman_spec_reply", 1,
+            &RomanJointTrajectoryController::romanSpecReplyCallback, this);
+
+    m_server.registerGoalCallback(boost::bind(&RomanJointTrajectoryController::goalCallback, this));
+    m_server.registerPreemptCallback(boost::bind(&RomanJointTrajectoryController::preemptCallback, this));
+
+    m_joint_name_to_spec_index = {
+        { "limb_right_joint1", 0 },
+        { "limb_right_joint2", 1 },
+        { "limb_right_joint3", 2 },
+        { "limb_right_joint4", 3 },
+        { "limb_right_joint5", 4 },
+        { "limb_right_joint6", 5 },
+        { "limb_right_joint7", 6 },
+        { "limb_left_joint1", 7 },
+        { "limb_left_joint2", 8 },
+        { "limb_left_joint3", 9 },
+        { "limb_left_joint4", 10 },
+        { "limb_left_joint5", 11 },
+        { "limb_left_joint6", 12 },
+        { "limb_left_joint7", 13 },
+        { "torso_joint1", 14 },
+        { "track_left_joint ", 15 },
+        { "track_right_joint", 16 }
+    };
+}
+
+RomanJointTrajectoryController::~RomanJointTrajectoryController()
+{
+    if (m_server.isActive()) {
+        m_server.setAborted();
+    }
+    m_server.shutdown();
+}
+
+int RomanJointTrajectoryController::run()
+{
+    m_server.start();
+    ros::spin();
+    return 0;
+}
+
+void RomanJointTrajectoryController::romanStateCallback(
+    const roman_client_ros_utils::RomanState::ConstPtr& msg)
+{
+    m_state = msg;
+}
+
+void RomanJointTrajectoryController::goalCallback()
+{
+    ROS_INFO("This is probably where i would publish the spec");
+    m_goal = m_server.acceptNewGoal();
+
+    // TODO: there's a possibility we might have to check here for whether a
+    // preempt has been requested between the time the callback was serviced
+    // and the goal was accepted (though this should be a very rare race condition)
+
+    for (auto& s : m_goal->trajectory.joint_names) {
+        ROS_INFO("%s", s.c_str());
+    }
+
+    if (m_goal->trajectory.points.size() > roman_client_ros_utils::RomanSpec::max_waypoints) {
+        ROS_ERROR("that's too many points");
+        // we could resample here or something smarter if needed
+        m_result.error_code = FollowJointTrajectoryActionServer::Result::INVALID_GOAL;
+        m_server.setAborted(m_result);
+        return;
+    }
+
+#if BUILD_LIVE_JPL
+    roman_client_ros_utils::RomanSpec path_msg;
+    path_msg.utime = ros::Time::now().toNSec() / 1e3;
+    path_msg.num_mechanisms = ROBOT_NUM_MECHS;
+    for (int m = 0; m < ROBOT_NUM_MECHS; ++m) {
+        path_msg.valid.push_back(false);
+    }
+    path_msg.valid[ROBOT_MECH_LIMB_1] = true;
+    path_msg.num_waypoints = m_goal->trajectory.points.size();
+    for (const trajectory_msgs::JointTrajectoryPoint& pt : m_goal->trajectory.points) {
+        roman_client_ros_utils::RomanWaypoint waypoint;
+
+        waypoint.utime = pt.time_from_start.toNSec() / 1e3;
+
+        waypoint.num_joints = ROBOT_NUM_JOINTS;
+
+        std::vector<double> positions(m_joint_name_to_spec_index.size(), 0.0);
+
+        for (size_t i = 0; i < m_goal->trajectory.joint_names.size(); ++i) {
+            const std::string& joint_name = m_goal->trajectory.joint_names[i];
+            double pos = pt.positions[i];
+            positions[m_joint_name_to_spec_index[joint_name]] = pos;
+        }
+        waypoint.positions = std::move(positions);
+
+        waypoint.world2robot.orientation.w = 1.0;
+
+        path_msg.waypoints.push_back(waypoint);
+    }
+//        ROS_INFO_STREAM(path_msg);
+    spec_update_times(path_msg);
+    m_roman_spec_pub.publish(path_msg);
+
+    if (!path_msg.waypoints.empty()) {
+        int duration_us = path_msg.waypoints.back().utime;
+        double duration_s = 3.0 * (double)duration_us / 1e6; // 2 inflation
+        ROS_INFO("Sleep for %d us (%0.3f seconds)", duration_us, duration_s);
+        ros::Duration(duration_s).sleep();
+        ROS_INFO("Done sleeping");
+    }
+
+    m_result.error_code = FollowJointTrajectoryActionServer::Result::SUCCESSFUL;
+    m_server.setSucceeded(m_result);
+#endif
+}
+
+void RomanJointTrajectoryController::preemptCallback()
+{
+    ROS_INFO("Preempt action goal");
+    // TODO: send halt message to the arm
+    m_server.setPreempted();
+}
+
+void RomanJointTrajectoryController::romanSpecReplyCallback(
+    const roman_client_ros_utils::RomanSpecReply::ConstPtr& msg)
+{
+    ROS_INFO("Received spec reply");
+    if (m_server.isActive()) {
+        if (!msg->attempted || msg->in_fault) {
+            m_result.error_code = FollowJointTrajectoryActionServer::Result::INVALID_GOAL;
+            m_server.setAborted(m_result);
+        }
+        else {
+            m_result.error_code = FollowJointTrajectoryActionServer::Result::SUCCESSFUL;
+            m_server.setSucceeded(m_result);
+        }
+    }
+}
